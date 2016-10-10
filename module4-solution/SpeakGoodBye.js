878c9c18506880fb3001a57ee23c2eedd0e24a87
tonycjr <tonychujr@gmail.com>
2016-10-10 15:36:06 -0700

    Update SpeakGoodBye.js

--- module4-solution/SpeakGoodBye.js
+++ module4-solution/SpeakGoodBye.js
@@ -18,8 +18,8 @@
 // See Lecture 52, part 2
 (function (window) {
   var byeSpeaker = {};
-  byeSpeaker.name = "Yaakov";
-  var greeting = "Good bye ";
+  byeSpeaker.name = "";
+  var greeting = "Good Bye ";
   byeSpeaker.speak = function () {
     console.log(greeting + byeSpeaker.name);
   }
