0a8282f7e5c78507f6e07e805149469758c390f4
tonycjr <tonychujr@gmail.com>
2016-10-10 11:49:16 -0700

    Update script.js

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -34,13 +34,15 @@ WARNING!!! WARNING!!!
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
 
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+/// var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 // STEP 10:
 // Loop over the names array and say either 'Hello' or "Good Bye"
 // using the 'speak' method or either helloSpeaker's or byeSpeaker's
 // 'speak' method.
 // See Lecture 50, part 1
+
+/*
 for (/* fill in parts of the 'for' loop to loop over names array */) {
 
   // STEP 11:
@@ -63,3 +65,106 @@ for (/* fill in parts of the 'for' loop to loop over names array */) {
     // helloSpeaker.xxxx
   }
 }
+*/
+     
+// Test Out
+// Arrays
+/*var array = new Array();
+array[0] = "Yaakov";
+array[1] = 2;
+array[2] = function (name) {
+  console.log("Hello " + name);
+};
+array[3] = {course: " HTML, CSS & JS"};
+
+console.log(array);
+array[2](array[0]);
+console.log(array[3].course);
+
+*/
+// Short hand array creation
+//var names = ["Yaakov", "John", "Joe"];
+
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+//console.log(names);
+
+for (var i = 0; i < names.length; i++) {
+
+  var firstLetter = names[i].charAt(0).toLowerCase();
+
+  // STEP 12: (NOTHING TO DO. ALREADY DONE FOR YOU)
+  // Compare the 'firstLetter' retrieved in STEP 11 to lower case
+  // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
+  // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
+  // name in the loop.
+  if (firstLetter === 'j') {
+    console.log("Bye " + names[i]);
+  } else {
+    console.log("Hello " + names[i]);
+  }
+
+}
+
+/*names[100] = "Jim";
+for (var i = 0; i < names.length; i++) {
+  console.log("Hello " + names[i]);
+}*/
+
+/*var names2 = ["Yaakov", "John", "Joe"];
+
+var myObj = {
+  name: "Yaakov",
+  course: "HTML/CSS/JS",
+  platform: "Courera"
+};
+for (var prop in myObj) {
+  console.log(prop + ": " + myObj[prop]);
+}
+
+for (var name in names2) {
+  console.log("Hello " + names2[name]);
+}
+
+names2.greeting = "Hi!";
+
+for (var name in names2) {
+  console.log("Hello " + names2[name]);
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
